14d5800fc07e9bbe348c55d8ed2a36377dee61e8
AmAgVa <144016257+AmAgVa@users.noreply.github.com>
2023-10-18 16:40:52 +0200

    Redirecting output for exercise (1.4)(2)

--- EG_v2.C
+++ EG_v2.C
@@ -18,6 +18,9 @@ void rootfuncgenerate(Int_t nEvents, Int_t nTracks, Double_t v2)
 {
   cout << "Generating " << nEvents << " events" << endl << endl;
 
+  // open output file
+  ofstream outputFile("phi_dist.dat");
+
   // create histogram that we will fill with random values
   TH1D* hPhi = new TH1D("hPhi", "Generated distribution; #phi; Counts", 
                         100, 0, TMath::TwoPi());
@@ -28,13 +31,16 @@ void rootfuncgenerate(Int_t nEvents, Int_t nTracks, Double_t v2)
   Double_t phi[nTracks]; // array to store phi angles
 
   for (Int_t ne = 0; ne < nEvents; ne++) {
-    cout << "Event " << ne << endl;
-    cout << "nTracks " << nTracks << endl;
+    outputFile << "Event " << ne << endl;
+    outputFile << "nTracks " << nTracks << endl;
     for (Int_t nt = 0; nt < nTracks; nt++) {
       Double_t phi = functionf->GetRandom();
-      cout << nt << " : " << phi << endl;
+      outputFile << nt << " : " << phi << endl;
     }
   }
+
+  // close file
+  outputFile.close();
 
   // make a loop for the number of events
   for(Int_t i = 0; i < nTracks; i++) {
@@ -50,7 +56,7 @@ void rootfuncgenerate(Int_t nEvents, Int_t nTracks, Double_t v2)
   hPhi->SetMinimum(0);
   hPhi->Draw();
   
-  // create 1d function that we will use to fit our generated data to determine Ψ2
+  // fitting function
   TF1* fitFunc = new TF1("fitFunc", "[0]*(1 + 2*[1]*cos(2*(x - 0)))", 0, TMath::TwoPi());
   fitFunc->SetParameter(0, v2);
   fitFunc->SetParLimits(1, 0, 1);
